7fc0a62c5aac9b62e668c25c9b77a614aac0a7f8
michael-gately <michaeldavidgately@gmail.com>
2024-04-09 12:32:06 -0700

    Fix emoji

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ style: "normal"
 export default function Home() {
 return (
 <main className="md:pl-0 pt-8 pr-16 text-center">
-  <div className={rubik.className.concat(" ", " pt-32 pl-32 pr-32 pb-8 text-4xl")}> 👋 Hey I'm Mike. </div>
+  <div className={rubik.className.concat(" ", " pt-32 pl-32 pr-32 pb-8 text-4xl")}> &#128075; Hey I'm Mike. </div>
   <div className={rubik.className.concat(" ", " text-base pb-8")}></div>
   <button className={bodyPoppins.className.concat(" "," bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded")}>About Me</button>
 </main>
