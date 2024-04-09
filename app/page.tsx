de00be64a36bedc447af8880f7e703a8e725a99c
michael-gately <michaeldavidgately@gmail.com>
2024-04-09 12:57:25 -0700

    fixing responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
 return (
 <main className="md:pl-0 pt-8 pr-16 text-center">
   <div className={rubik.className.concat(" ", " pt-32 pl-32 pr-32 pb-8 text-4xl")}> &#128075; Hi I&apos;m Mike </div>
-  <div className={rubik.className.concat(" ", " text-base pb-8")}>A subject matter expert in Backend Software Development</div>
+  <div className={rubik.className.concat(" ", " text-base pl-32 pr-32 pb-8")}>A subject matter expert in Backend Software Development</div>
   <Link href="/skills">
-    <button className={bodyPoppins.className.concat(" "," bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-80")}>Skills</button>
+    <button className={bodyPoppins.className.concat(" ","ml-32 mr-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded mb-80")}>Skills</button>
   </Link>
 </main>
 )
