56a8ec389df0be5c9669be27c1d814d85d73b158
michael-gately <michaeldavidgately@gmail.com>
2024-04-10 11:51:57 -0700

    Fixing responsiveness on home

--- app/page.tsx
+++ app/page.tsx
@@ -17,14 +17,14 @@ style: "normal"
 export default function Home() {
 return (
 <main className="md:pl-0 pt-8 text-left">
-  <div className={rubik.className.concat(" ", " pt-32 pl-32 pr-32 pb-8 text-4xl")}> &#128075; Hi I&apos;m Mike </div>
-  <div className={rubik.className.concat(" ", " text-base pl-32 pr-32 pb-8")}>A subject matter expert in Backend Software Development</div>
-  <div className="pl-32">
+  <div className={rubik.className.concat(" ", " pt-32 pl-8 pr-8 lg:pl-32 lg:pr-32 pb-8 text-4xl")}> &#128075; Hi I&apos;m Mike </div>
+  <div className={rubik.className.concat(" ", " text-base pl-8 pr-8 lg:pl-32 lg:pr-32 pb-8")}>A subject matter expert in Backend Software Development</div>
+  <div className="pl-8 flex flex-col lg:flex-row lg:pl-32">
     <Link href="/skills">
-      <button className={bodyPoppins.className.concat(" "," bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded mb-80")}>Skills</button>
+      <button className={bodyPoppins.className.concat(" "," bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded mb-8 lg:mb-80")}>Skills</button>
     </Link>
     <Link href="/work-history">
-    <button className={bodyPoppins.className.concat(" "," ml-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded mb-80")}>Experience</button>
+    <button className={bodyPoppins.className.concat(" "," lg:ml-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded mb-80")}>Experience</button>
     </Link>
   </div>
   
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -16,20 +16,20 @@ return (
     </div>
     <div className='md:grid grid-cols-4 pt-8'>
         <SkillCard source="file-type-python.svg" caption="My preferred backend coding language is Python.
-         But I also have experience in Go and Ruby" />
+         But I also have experience in Go and Ruby." />
         <SkillCard source="django.svg" caption="Experience with the Django Framework.
          Expecially with using the ORM and optimizing queries." />
         <SkillCard source="github.svg" caption="Knowledge of version control and
-         peer code review using Git and Github" />
+         peer code review using Git and Github." />
         <SkillCard source="vs-code.svg" caption="I use VS code as my main IDE and have
-         a good knowledge of using useful extentions like pytest and beautify"/>
+         a good knowledge of using useful extentions like pytest and beautify."/>
         <SkillCard source="linux.svg" caption="Experience with Linux based systems and command line.
-         I know my way around a man page" />
+         I know my way around a man page." />
         <SkillCard source="elasticsearch.svg" caption="Debugging with logs in Elastic Search to 
         find bugs and solve user issues." />
         <SkillCard source="sql.svg" caption="Knowledge of SQL queries and database structure." />
         <SkillCard source="celery.svg" caption="Use to using Celery as an asynchronous event 
-        bus to run blocks of code on a cron schedule" />
+        bus to run blocks of code on a cron schedule." />
     </div>
 </div>
 )
