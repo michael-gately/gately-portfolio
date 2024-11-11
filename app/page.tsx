5f7ff48a65f5ec09ec5b7644a167c5b68442bc43
Michael Gately <michaeldavidgately@gmail.com>
2024-11-11 07:56:44 -0800

    home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,11 @@ const lowerPoppins = Poppins({
 export default function Home() {
 return (
 <main className="text-lg">
+  <div className='flex gap-5 pb-10'>
+    <a href="/" className='text-lg'>Home</a>
+    <a href="/projects" className='text-lg'>Projects</a>
+    <a href="https://github.com/michael-gately" className='text-lg'>Github</a>
+  </div>
   <div className='flex'>
     <img className="border-2" src='gately-mike.jpg'/>
       <div className='pl-5 col-span-2'>
@@ -35,16 +40,17 @@ return (
   </div>
   <div className='pt-10 flex-col'>
     <p className='text-4xl pb-5'>About Me</p>
-    <p className='pb-5'>As a developer working at STRATIS IoT, I am learning new skills every day. 
-      While building internal tools, bolstering current integrations, and assisting with developing new features, my skill set is growing exponentially. 
-      I have increased my knowledge with everything full stack like Typescript, React-redux, Kubernetes, Azure, Python, gRPC, graphQL, Django ORM, and Go. 
-      I am looking forward to continuing to advance my software engineering skills!</p>
+    <p className='pb-5'>With three years of experience as a software engineer at STRATIS IoT,
+      I have developed a strong and diverse technical skill set, especially in backend development.
+      I regularly build internal tools, resolve complex bugs, and contribute to new feature development,
+      deepening my expertise in technologies such as Python, TypeScript, React-Redux, Kubernetes, Azure,
+      gRPC, GraphQL, and Django ORM. I am dedicated to continuously advancing my skills and making impactful contributions to the team.</p>
     <p className='text-4xl pb-5'>Experience</p>
-    <p className=''>Stratis IoT - Backend Python Developer II</p>
+    <p className=''>STRATIS IoT - Backend Python Developer II</p>
     <ul className='list-disc pl-5'>
       <li>Experienced in optimizing database queries and managing data models using Django ORM for efficient data retrieval and storage.</li>
       <li>Proven track record of delivering high-quality code following best practices such as code reusability, modularity, and maintainability in Python and Django environments.</li>
-      <li>Proficient in applying Domain-Driven Design principles to architect and develop backend solutions, ensuring alignment with business requirements and promoting maintainability, scalability, and clarity in codebases.</li>
+      <li>Proficient in applying Domain-Driven Design principles to architect and develop backend solutions, ensuring alignment with business requirements and promoting maintainability, scalability, and clarity.</li>
       <li>Skilled in designing and implementing RESTful APIs using Django Rest Framework to facilitate seamless communication between front-end and back-end systems. </li>
     </ul>
     <p className='pt-2'>Northrop Grumman - Software Engineer Intern</p>
@@ -57,8 +63,7 @@ return (
     <p className='pt-2'>Computer Engineering Major - Bachelor of Science - San Diego State University</p>
     <ul className='list-disc pl-5 pb-5'>
       <li>Studied a wide range of computer principles like Computer Architecture, Embedded Systems, and Computer Logic.</li>
-      <li>Computer Science Minor.</li>
-      <li>I was able to learn a lot about both software and hardware</li>
+      <li>Computer Science Minor that let me learn more about data structures, algorithms, and system administration.</li>
     </ul>
   </div>
 </main>
