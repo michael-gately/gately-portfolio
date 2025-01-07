2f64de6cb1687c5a52b6aa4e9423063b730f9832
Michael Gately <michaeldavidgately@gmail.com>
2025-01-07 07:55:05 -0800

    Fix top bar on projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,11 +9,16 @@ style: "normal"
 
 export default function Projects() {
 return (
-<div className='pl-8 md:pl-0 pt-8 pr-16'>
-    <div className={bodyPoppins.className.concat(" ", " text-left text-base md:text-lg")}>
-        Athlete&apos;s Grind Automation
+<div className=''>
+    <div className='flex gap-5 pb-10'>
+        <a href="/" className='text-lg'>Home</a>
+        <a href="/projects" className='text-lg'>Projects</a>
+        <a href="https://github.com/michael-gately" className='text-lg'>Github</a>
     </div>
-    <div className={bodyPoppins.className.concat(" ", " pt-8 text-left text-sm md:text-base")}>
+    <div className={bodyPoppins.className.concat(" ", " text-left text-base md:text-lg")}>
+        1. Athlete&apos;s Grind Automation
+    </div>
+    <div className={bodyPoppins.className.concat(" ", " pl-4 pt-4 text-left text-sm md:text-base")}>
         Automated a coffee company&apos;s wholesale fulfilling process through a hosted Django app.
         The business needed a way to automatically send emails to a distributor,
         automatically generate a shipping label depending on goods ordered, and
@@ -22,7 +27,7 @@ return (
         with an external Shipping Label API, MIME email generation, 
         and pdfkit to create a purchase order to attach.
     </div>
-    <video width="800" height="400" controls preload="auto" className='text-center pt-5'>
+    <video width="800" height="400" controls preload="auto" className='text-center pl-4 pt-5'>
         <source src="AGDemo2.m4v" type="video/mp4" />
     </video>
 </div>
